f22e8a1f03a8b56272eef1e8d5b08272fa351bd9
MamunHossen1998 <eng.rubelkhan797991@gmail.com>
2024-12-16 00:38:52 +0600

    config issue

--- app/Http/Controllers/TestWebController.php
+++ app/Http/Controllers/TestWebController.php
@@ -9,16 +9,17 @@ class TestWebController extends Controller
     public function test(){
         return 'test';
     }
-<<<<<<< HEAD
+
     public function testDataDetails(){
         return 'test details';
     }
     public function testController(){
         return "test controller";
-=======
+    }
+
     public function testData()
     {
         return "Test Data";
->>>>>>> 8003b5683cde8d4baa1b2ade3dff5c84ff4bd98b
+
     }
 }
