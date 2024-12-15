518e1f34e4724ae1b0e3c4b87b306a1aa54321e3
MamunHossen1998 <eng.rubelkhan797991@gmail.com>
2024-12-15 12:19:50 +0600

    test details

--- app/Http/Controllers/TestWebController.php
+++ app/Http/Controllers/TestWebController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class TestWebController extends Controller
 {
-    //
+    public function test(){
+        return 'test';
+    }
+    public function testDataDetails(){
+        return 'test details';
+    }
 }
