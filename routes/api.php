8f1dfa87f67f3ac4fea91af649a73e6a2d489a54
MamunHossen1998 <eng.rubelkhan797991@gmail.com>
2024-12-22 23:32:16 +0600

    create product controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,4 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['prefix' => 'v1'], function () {
     Route::resource('/user', UserApiController::class);
     Route::post('/user/login',[LoginController::class,'userLogin']);
+    Route::resource('/product',ProductController::class);
 });
